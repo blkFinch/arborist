46359fe240b752c80056c5ec0ac40987fccd1c07
finch <blkfinch@gmail.com>
2023-10-16 09:20:11 -0500

    init commit aborist

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world styled with tailwind
+      </h1>
+      <p className='italic'>some text</p>
+    </>
+  )
+}
+
+export default App
