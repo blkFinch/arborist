39b6bb76f82a6507a361c38cacd38c668f9426f0
Galen Hutchison <galen.hutchison@beam.dental>
2023-12-14 16:33:14 -0600

    add redux tools, add child

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from "electron";
+import installExtension, { REDUX_DEVTOOLS } from "electron-devtools-installer";
 import path from "node:path";
 
 // The built directory structure
@@ -58,6 +59,13 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+//Install dev tools
+app.whenReady().then(() => {
+  installExtension(REDUX_DEVTOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log("An error occurred: ", err));
 });
 
 app.whenReady().then(createWindow);
---
+++ src/store/Document.ts
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Node } from "../utils/DataStuctures";
+
+interface DocumentState {
+  title: string;
+  nodes: NodeState[];
+  activeNodeId: string | null;
+}
+
+export interface NodeState {
+  id: string;
+  prev: string | null; //deprecated
+  next: string | null; //deprecated
+  children: string[];
+  data: string;
+}
+
+export interface BranchState {
+  nodeIds: string[];
+}
+
+const initialState: DocumentState = {
+  nodes: [],
+  activeNodeId: null,
+  title: "Untitled",
+};
+
+export const documentSlice = createSlice({
+  name: "stem",
+  initialState,
+  reducers: {
+    createNode: (state, action) => {
+      const node = new Node(action.payload);
+      const nodeData: NodeState = node.getData();
+      state.activeNodeId = nodeData.id;
+      state.nodes.push(nodeData);
+    },
+    removeNode: (state, action) => {
+      const index = state.nodes.findIndex((node) => node.id === action.payload);
+      const prevNode = state.nodes[index - 1];
+      state.nodes.splice(index, 1);
+      state.activeNodeId = prevNode?.id || null;
+    },
+    setActiveNode: (state, action) => {
+      state.activeNodeId = action.payload;
+    },
+    createChildNode: (state, action) => {
+      const node = new Node(action.payload);
+      const nodeData: NodeState = node.getData();
+      const index = state.nodes.findIndex(
+        (node) => node.id === state.activeNodeId
+      );
+      state.nodes[index].children.push(nodeData.id);
+    },
+  },
+});
+
+export const { createNode, removeNode, setActiveNode, createChildNode } =
+  documentSlice.actions;
+export default documentSlice.reducer;
